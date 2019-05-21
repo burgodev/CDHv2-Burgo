16c3f263da89bcee6012703d450ca98c3562edc7
Filipe Burgonovo <burgodev@gmail.com>
2019-05-21 12:49:51 -0300

    home deleted

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import AdminCDH from './views/AdminCDH.vue'
 import UsersBurgo from './views/UsersBurgo.vue'
@@ -15,12 +14,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-
     {
       path: '/login',
       name: 'login',
