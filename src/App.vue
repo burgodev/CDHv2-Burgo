7b61464a586bc63724bed389ccb8a876efb771d2
Filipe Burgonovo <burgodev@gmail.com>
2019-05-21 12:46:17 -0300

    about deleted

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <v-app  id="app">
     <div id="nav" >
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/login2">Login2</router-link> |
       <router-link to="/timeRegister">CDH</router-link>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,96 +1,148 @@
 <template>
     <v-container fluid fill-height>
-        <v-layout align-content-center column>
-            <v-flex></v-flex>
-            <v-flex>
-                <v-card
-                        max-width="344"
-                        class="mx-auto"
-                        style="background-color: #711f1f"
-                >
-                    <v-card-title>
-                        <v-layout justify-center>
-                            <span class="display-1" style="font-weight: bold; color: whitesmoke">Login</span>
-                        </v-layout>
-                    </v-card-title>
-                    <v-card ref="form">
-                        <v-card-text>
-                            <v-text-field
-                                    ref="name"
-                                    v-model="name"
-                                    :rules="[() => !!name || 'This field is required']"
-                                    :error-messages="errorMessages"
-                                    label="Full Name"
-                                    placeholder="Digite o seu nome"
-                                    required
-                            ></v-text-field>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-card-text>
+                    <v-text-field dark
+                                  ref="name"
+                                  v-model="user.login"
+                                  color="primary"
+                                  placeholder="Username"
+                                  required
+                    ></v-text-field>
 
-                            <v-text-field
-                                    v-model="password"
-                                    :append-icon="show1 ? 'visibility' : 'visibility_off'"
-                                    :rules="[rules.required, rules.min]"
-                                    :type="show1 ? 'text' : 'password'"
-                                    name="input-10-1"
-                                    label="Normal with hint text"
-                                    hint="At least 8 characters"
-                                    counter
-                                    @click:append="show1 = !show1"
-                            ></v-text-field>
+                    <v-text-field dark
+                                  v-model="user.password"
+                                  :append-icon="show1 ? 'visibility' : 'visibility_off'"
+                                  :type="show1 ? 'text' : 'password'"
+                                  placeholder="Password"
+                                  name="input-10-1"
+                                  color="primary"
+                                  counter
+                                  @click:append="show1 = !show1"
+                    ></v-text-field>
+                </v-card-text>
+
+                <v-spacer></v-spacer>
+                <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      class="my-0"
+                      @click="resetForm"
+                      v-on="on"
+                    >
+                        <v-icon>refresh</v-icon>
+                    </v-btn>
+                </template>
 
 
-                        </v-card-text>
-                        <v-divider class="mt-5"></v-divider>
-                        <v-card-actions>
-                            <v-btn text>Cancel</v-btn>
-                            <v-spacer></v-spacer>
-                            <v-slide-x-reverse-transition>
-                                <v-tooltip
-                                        v-if="formHasErrors"
-                                        left
-                                >
-                                    <template v-slot:activator="{ on }">
-                                        <v-btn
-                                                icon
-                                                class="my-0"
-                                                @click="resetForm"
-                                                v-on="on"
-                                        >
-                                            <v-icon>refresh</v-icon>
-                                        </v-btn>
-                                    </template>
-                                    <span>Refresh form</span>
-                                </v-tooltip>
-                            </v-slide-x-reverse-transition>
-                            <v-btn color="primary" text @click="submit">Submit</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-card>
+                <v-btn class="custom-btn" @click="login" outline round small text>Login</v-btn>
+
+
             </v-flex>
-            <v-flex></v-flex>
         </v-layout>
+
+
     </v-container>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
-    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import {
+        OpenAPI,
+    } from '../requests';
+
+
+
 
     @Component({
-        components: {
-            HelloWorld,
-        },
+        components: {},
     })
-    export default class Login extends Vue {
-      data () {
-        return {
-          show1: false,
-          password: 'Password',
-          rules: {
-            required: value => !!value || 'Required.',
-            min: v => v.length >= 8 || 'Min 8 characters',
-            emailMatch: () => ('The email and password you entered don\'t match')
-          }
+    export default {
+
+        data() {
+            return {
+                show1: false,
+                user: {
+                    login: 'admin@admin.com',
+                    password: 'admin',
+                },
+            }
+        },
+
+        methods: {
+
+            async login() {
+                let ret = await OpenAPI.login({login: this.user.login, password: this.user.password});
+
+                if (ret.success) {
+                    localStorage.setItem('authenticationKey', ret.data.authenticationKey);
+                    localStorage.setItem('accessKey', ret.data.accessKey);
+                    localStorage.setItem('id', ret.data.id);
+                    localStorage.setItem('name', ret.data.name)
+
+
+                    this.$router.replace('/AdminCDH')
+
+
+
+                    // this.$refs.AppBurgo.showCdh();
+
+
+
+
+                    //
+                    // let AppBurgo = new AppBurgo();
+                    //
+                    // AppBurgo.showCdh();
+
+
+                }
+
+
+                // try {
+                // let ret = await axios.post('http://localhost:1337/api/login', this.user);
+                //
+                // if (ret.data.success) {
+                //   localStorage.setItem('authenticationKey', ret.data.data.authenticationKey);
+                //   localStorage.setItem('accessKey', ret.data.data.accessKey);
+                //   localStorage.setItem('accessKey', ret.data.data.id);
+                //
+                //   } else
+                //     alert(ret.data.data.description);
+                //
+                //   // console.log('retornou isso', ret);
+                //
+                //
+                // } catch (e) {
+                //   console.error('erro no login', e);
+                // }
+
+
+            },
         }
-      }
     }
+
+
 </script>
+
+<style>
+
+    /*.custom-btn::before {*/
+    /*color: transparent*/
+    /*}*/
+
+
+    /*.timeRegister-color {*/
+    /*background-image: radial-gradient(#444e54, #0b0b0b)*/
+    /*}*/
+
+
+    /*.login-card {*/
+    /*style="background-color: rgba(244,248,255,0.8)"*/
+    /*}*/
+
+
+</style>
+
+
